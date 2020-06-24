ddbf20008b0d8a88dc1b93c51b0e3f02e901218a
Anner <anner.calvo@gmail.com>
2020-06-23 21:40:12 -0600

    Read each document, not only the file

--- Programada-2/src/main/main.java
+++ Programada-2/src/main/main.java
@@ -5,6 +5,11 @@
  */
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,6 +25,7 @@ import org.tartarus.snowball.ext.PorterStemmer;
 import org.tartarus.snowball.ext.SpanishStemmer;
 
 import modules.Normalization;
+import modules.URL;
 
 /**
  *
@@ -47,7 +53,33 @@ public class main {
     public static void main(final String[] args) throws IOException {
         Normalization aux = new Normalization();
         String text = "Los cuentos clásicos son parte de nuestra cultura, ya que enseñan lecciones y consejos a los más pequeños desde hace siglos. Es por ello que Mundo Primaria te trae una selección de los mejores cuentos clásicos, para que tanto tú como tu hijo o hija disfrutéis de estos relatos que contienen una sabiduría tan importante que se ha seguido transmitiendo con el paso de los años";
-        aux.readText();
+        URL path = new URL();
+        aux.startIndization();
+
+  
+        //  // Creamos un arhivo vacio indicando un texto para el inicio y final 
+        //  // (prefijo y sufijo) del archivo. Muy util para saber cuales son nuestro
+        //  // archivos temporales
+        //  File f = File.createTempFile("archivoTemporal_",".lwp");
+        //  System.out.println("Path: " + f.getAbsolutePath() );
+  
+        //  // guardamos texto en el archivo temporal
+        //  BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        //  bw.write(linea);
+        //  bw.close();
+  
+        //  // leemos cada linea del archivo temporal
+        //  String str;
+        //  BufferedReader br = new BufferedReader(new FileReader(f));
+        //  while((str = br.readLine()) != null) {
+        //      System.out.println("Texto guardado en el archivo temporal: "+str);
+        //  }
+        //  br.close();
+  
+        //  // Eliminamos el archivo temporal cuando ya no lo necesitemos
+        //  f.delete();
+        
+        // aux.readText();
 
         // System.out.println(text);
         // Analyzer analyzer = CustomAnalyzer.builder()
--- Programada-2/src/modules/Normalization.java
+++ Programada-2/src/modules/Normalization.java
@@ -16,7 +16,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.Normalizer;
 
@@ -88,21 +90,20 @@ public class Normalization {
       return cleanText;
     }
 
-    public void readText() throws IOException {
-      readTitle();
-      // readA();
-      // readH();
-      // readBody();
+    public void readText(String path) throws IOException {
+      readTitle(path);
+      readA(path);
+      readH(path);
+      readBody(path);
     }
 
 
 
-    public void readLabel(String label, ArrayList<String> toIndex) throws IOException{
+    public void readLabel(String label, ArrayList<String> toIndex, String path) throws IOException{
       String text="";
         
-      final URL path = new URL();
         // load file
-        final File inputFile = new File(path.pathp2);
+      final File inputFile = new File(path);
       // parse file as HTML document
       final Document doc = Jsoup.parse(inputFile, "UTF-8");
       // select element by <title>
@@ -122,25 +123,24 @@ public class Normalization {
       toIndex.forEach(System.out::println);
     }
 
-    public void readTitle() throws IOException {
-       readLabel("title", toIndexTitle);
+    public void readTitle(String path) throws IOException {
+       readLabel("title", toIndexTitle, path);
     }
 
-    public void readA() throws IOException {
-      readLabel("a", toIndexA);
+    public void readA(String path) throws IOException {
+      readLabel("a", toIndexA, path);
     }
 
-    public void readBody() throws IOException {
-      readLabel("body", toIndexBody);
+    public void readBody(String path) throws IOException {
+      readLabel("body", toIndexBody, path);
     }
 
 
-    public void readH() throws IOException {
+    public void readH(String path) throws IOException {
         String text="";
         
-        final URL pathp2 = new URL();
         // load file
-        final File inputFile = new File(pathp2.pathp2);
+        final File inputFile = new File(path);
         // parse file as HTML document
         final Document doc = Jsoup.parse(inputFile, "UTF-8");
         for(int i=1; i<=5; i++){
@@ -161,6 +161,40 @@ public class Normalization {
             }
         }
         toIndexH.forEach(System.out::println);
+    }
+
+    public void startIndization() throws IOException {
+      final URL pathp2 = new URL();
+      final File inputFile = new File(pathp2.pathp2);
+      FileReader  fr = new FileReader (inputFile);
+      BufferedReader  br1 = new BufferedReader(fr);
+
+      String texto="";
+
+      // Lectura del fichero
+      String linea;
+      while((linea=br1.readLine())!=null){
+          texto+=linea+"\n";
+      }
+      
+      String regex = "<!DOCTYPE html PUBLIC \"-\\/\\/W3C\\/\\/DTD XHTML 1.0 Transitional\\/\\/EN\" \"http:\\/\\/www.w3.org\\/TR\\/xhtml1\\/DTD\\/xhtml1-transitional.dtd\">\\n<.*>((.*|\\n)*?)<\\/html>";
+      Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+      Matcher matcher = pattern.matcher(texto);
+      int con=1;
+      while (matcher.find() && con<=1) {
+        con++;
+
+        File f = File.createTempFile("archivoTemporal_",".lwp");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write(matcher.group(0));
+        bw.close();
+        readText(f.getAbsolutePath());
+        f.delete();
+
+        System.out.println();
+        System.out.println("aquí inicia la otra");
+        System.out.println();
+      }
     }
 
     
